c79803ffdcbdb2a19b9d8e48a3f785d540eb637b
Jakub Kozłowski <kubukoz@gmail.com>
2022-08-26 19:03:51 +0200

    init

---
+++ main.scala
@@ -0,0 +1,44 @@
+//> using scala "2.13.8"
+//> using lib "com.kubukoz::debug-utils:1.1.3"
+import util.chaining._
+import com.kubukoz.DebugUtils
+
+object main {
+
+  trait Lens[S, A] {
+    def update(s: S, a: A): S
+  }
+
+  case class User(name: String, age: Int)
+
+  object User {
+    val name: Lens[User, String] = ???
+    val age: Lens[User, Int] = ???
+  }
+
+  val u = User("hello", 400)
+
+  u
+    .pipe(User.name(_) = "foo")
+    .pipe(User.age(_) = 42)
+
+}
+
+object main2 {
+
+  trait Setter[S, A]
+
+  case class user(name: String, age: Int) {
+    // def name_=(v: String): User = ???
+    def name_=(newName: String): user = ???
+  }
+
+  object user {
+    def name_=(newName: String): user => user = ???
+  }
+
+  val u: user = ???
+
+  DebugUtils.withDesugar(u.name = "foo")
+
+}
