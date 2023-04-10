7152d94f313fb140b0114f37e12efd12ce01af5d
Jakub Kozłowski <kubukoz@gmail.com>
2023-04-10 20:59:15 +0200

    Add clock

--- main/src/app/main.scala
+++ main/src/app/main.scala
@@ -10,13 +10,19 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 import views.ProductListFetchView
 import views.ProductFetchView
 import Html._
+import scala.concurrent.duration._
+import scala.scalajs.js.Date
+import cats.implicits._
 
 enum Msg {
   case ProductMsg(msg: ProductFetchView.Msg)
   case ListMsg(msg: ProductListFetchView.Msg)
+
+  case Tick(date: Date)
 }
 
 case class Model(
+  now: Option[Date],
   product: ProductFetchView.Model,
   list: ProductListFetchView.Model,
 )
@@ -24,6 +30,7 @@ case class Model(
 object Model {
 
   val init: Model = Model(
+    now = None,
     product = ProductFetchView.Model.init,
     list = ProductListFetchView.Model.init,
   )
@@ -36,14 +43,17 @@ object App extends TyrianApp[Msg, Model] {
   override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
     (
       Model.init,
-      Cmd.None,
+      Cmd.Run(IO(new Date(Date.now())))(Msg.Tick(_)),
     )
 
   def subscriptions(model: Model): Sub[IO, Msg] =
-    ProductFetchView.subscriptions(model.product).map(translateProductMsg) |+|
+    Sub.every[IO](1.second).map(date => Msg.Tick(date)) |+|
+      ProductFetchView.subscriptions(model.product).map(translateProductMsg) |+|
       ProductListFetchView.subscriptions(model.list).map(translateListMsg)
 
   override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = {
+
+    case Msg.Tick(date) => (model.copy(now = date.some), Cmd.None)
 
     case Msg.ListMsg(msg) =>
       val (newListModel, listMsg) = ProductListFetchView.update(model.list)(msg)
@@ -63,21 +73,31 @@ object App extends TyrianApp[Msg, Model] {
 
   }
 
-  private def translateListMsg
+  private val translateListMsg
     : ProductListFetchView.Msg | ProductListFetchView.PropagatedMsg => Msg = {
     case ProductListFetchView.PropagatedMsg.ProductSelected(product) =>
       Msg.ProductMsg(ProductFetchView.Msg.ProductIdChanged(product.id))
     case msg: ProductListFetchView.Msg => Msg.ListMsg(msg)
   }
 
-  private def translateProductMsg: ProductFetchView.Msg => Msg = { msg =>
+  private val translateProductMsg: ProductFetchView.Msg => Msg = { msg =>
     Msg.ProductMsg(msg)
   }
 
   override def view(model: Model): Html[Msg] =
     div(className := "container")(
-      ProductFetchView.view(model.product).map(translateProductMsg(_)),
-      ProductListFetchView.view(model.list).map(translateListMsg(_)),
+      model
+        .now
+        .map { date =>
+          p(s"Current time: ${date.toLocaleTimeString()}")
+        }
+        .toList
+        .appendedAll(
+          List(
+            ProductFetchView.view(model.product).map(translateProductMsg(_)),
+            ProductListFetchView.view(model.list).map(translateListMsg(_)),
+          )
+        )
     )
 
 }
