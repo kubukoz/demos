9615e23befc15c6575356ab582994a745e157fda
Jakub Kozłowski <kubukoz@gmail.com>
2023-05-21 16:41:32 +0200

    format

--- src/test/scala/MyTest.scala
+++ src/test/scala/MyTest.scala
@@ -1,6 +1,7 @@
 import zio.test.Assertion._
+
 import zio.test._
 
 object MyTest extends ZIOSpecDefault {
-  def spec = test("example") { assertTrue(true) }
+  def spec = test("example")(assertTrue(true))
 }
