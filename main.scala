61c84b53590ae751e49b0fc57272b1fe23df6d40
Jakub Kozłowski <kubukoz@gmail.com>
2023-06-23 03:20:43 +0200

    traversal-less approach

--- main.scala
+++ main.scala
@@ -1,147 +1,106 @@
 //> using lib "org.typelevel::cats-core:2.9.0"
-//> using lib "dev.optics::monocle-core:3.2.0"
+//> using plugin "org.polyvariant:::better-tostring:0.3.17"
 //> using option "-no-indent"
-import cats.implicits.*
-import cats.Applicative
 
 import scala.collection.immutable.ListMap
 import scala.compiletime._
 import scala.deriving.Mirror
-import scala.reflect.ClassTag
+import scala.reflect.TypeTest
+
+trait Copy[T] {
+
+  /** Transform all pieces of this structure using the given function.
+    */
+  def copyWith(t: T)(f: Mod): T
+}
+
+type AnyTypeTest[T] = TypeTest[Any, T]
 
 object Copy {
 
-  inline given derived[T: ClassTag]: Copy[T] = summonFrom {
+  inline given derived[T: AnyTypeTest]: Copy[T] = summonFrom {
     case m: Mirror.SumOf[T] =>
       new Copy[T] {
+        lazy val instances = summonAll[Tuple.Map[m.MirroredElemTypes, Copy]].toArray
 
-        override def make[Of: ClassTag]: Traversal[T, Of] =
-          new Traversal[T, Of] {
+        override def copyWith(t: T)(f: Mod): T = {
+          val instance = instances(m.ordinal(t)).asInstanceOf[Copy[Any]]
 
-            private val defo = defaultInstance[T]
+          instance.copyWith(t)(f).asInstanceOf[T]
 
-            private lazy val childTraversals: Array[Traversal[Any, Of]] =
-              summonAll[Tuple.Map[m.MirroredElemTypes, Copy]]
-                .toArray
-                .map(_.asInstanceOf[Copy[Any]].make[Of])
-
-            override def modify(
-              f: Of => Of
-            )(
-              t: T
-            ): T =
-              defo.modifyAll(
-                childTraversals(m.ordinal(t)).modify(f)(t).asInstanceOf[T]
-              )(f)
-
-          }
+        }
 
       }
 
     case m: Mirror.ProductOf[T] =>
       new Copy[T] {
+        lazy val instances = summonAll[Tuple.Map[m.MirroredElemTypes, Copy]].toArray
 
-        override def make[Of: ClassTag]: Traversal[T, Of] =
-          new Traversal[T, Of] {
+        override def copyWith(t: T)(f: Mod): T = {
+          // for each member of the product, call copyWith on it
+          // then reconstruct the instance of T
+          // finally, apply f to the whole thing.
 
-            private val defo = defaultInstance[T]
+          val values =
+            m.fromProduct(t.asInstanceOf[Product]).asInstanceOf[Product].productIterator.toArray
 
-            private lazy val childTraversals =
-              summonAll[Tuple.Map[m.MirroredElemTypes, Copy]]
-                .toArray
-                .map(_.asInstanceOf[Copy[Any]].make[Of])
-                .zipWithIndex
-
-            override def modify(f: Of => Of)(s: T): T =
-              defo.modifyAll {
-                m
-                  .fromProduct {
-                    Tuple.fromArray {
-                      childTraversals
-                        .map { case (childInstance, index) =>
-                          val childField = s.asInstanceOf[Product].productElement(index)
-                          childInstance.modify(f)(childField)
-                        }
-                    }
-                  }
-              }(f)
-
+          val newValues = instances.zip(values).map { (copy, value) =>
+            val c = copy.asInstanceOf[Copy[Any]]
+            c.copyWith(value)(f)
           }
+
+          f(
+            m.fromProduct(Tuple.fromArray(newValues))
+          )
+
+        }
 
       }
 
     case _ =>
       // default instance: no mirror
-      defaultInstance[T]
+      new Copy[T] {
+        override def copyWith(t: T)(f: Mod): T = f(t)
+      }
   }
 
-  def defaultInstance[T: ClassTag]: Copy[T] =
-    new Copy[T] {
-
-      override def make[Of: ClassTag]: Traversal[T, Of] =
-        new Traversal[T, Of] {
-
-          override def modify(f: Of => Of)(s: T): T =
-            s match {
-              case of: Of =>
-                f(of) match {
-                  case t: T => t
-                  case _    => s // by some miracle the result wasn't a T
-                }
-              case _ => s
-            }
-
-        }
-
-    }
-
-  given [T: Copy]: Copy[List[T]] with {
-
-    override def make[Of: ClassTag]: Traversal[List[T], Of] = {
-      lazy val child = summon[Copy[T]].make[Of]
-
-      new Traversal[List[T], Of] {
-        override def modify(
-          f: Of => Of
-        )(
-          s: List[T]
-        ): List[T] = s.map(child.modify(f))
-      }
-    }
-
+  given [T: Copy: AnyTypeTest]: Copy[List[T]] with {
+    override def copyWith(t: List[T])(f: Mod): List[T] = t.map(_.copyWith(f))
   }
 
   given [K: Copy, V: Copy]: Copy[ListMap[K, V]] with {
 
-    override def make[Of: ClassTag]: Traversal[ListMap[K, V], Of] =
-      new Traversal[ListMap[K, V], Of] {
-        private val kC = summon[Copy[K]].make[Of]
-        private val vC = summon[Copy[V]].make[Of]
-
-        override def modify(
-          f: Of => Of
-        )(
-          s: ListMap[K, V]
-        ): ListMap[K, V] = s
-          .map { case (k, v) => (kC.modify(f)(k), vC.modify(f)(v)) }
-
-      }
+    override def copyWith(t: ListMap[K, V])(f: Mod): ListMap[K, V] = t.map { case (k, v) =>
+      (k.copyWith(f), v.copyWith(f))
+    }
 
   }
 
 }
 
-extension [T: Copy](t: T) {
-  def modifyAll[Of: ClassTag](f: Of => Of): T = summon[Copy[T]].modifyAll(t)(f)
+extension [T: Copy](t: T) def copyWith(f: Mod): T = summon[Copy[T]].copyWith(t)(f)
+
+trait Mod {
+  def apply[U: AnyTypeTest](a: U): U
 }
 
-trait Copy[T] {
-  def make[Of: ClassTag]: Traversal[T, Of]
-  def modifyAll[Of: ClassTag](t: T)(f: Of => Of): T = make[Of].modify(f)(t)
-}
+object Mod {
 
-trait Traversal[S, T] {
-  def modify(f: T => T)(s: S): S
+  def collect[T: AnyTypeTest](f: T => T): Mod =
+    new Mod {
+
+      override def apply[U: AnyTypeTest](a: U): U =
+        a match {
+          case t: T =>
+            f(t) match {
+              case b: U => b
+              case _    => a // return unmodified
+            }
+          case _ => a
+        }
+
+    }
+
 }
 
 // demo
@@ -169,10 +128,12 @@ enum RecIdent {
   case Just(id: String, i: Identifier)
 }
 
-def censorIdentifiers[T: Copy](t: T): T = t.modifyAll[Any] {
-  case id: Identifier => id.copy(name = Console.GREEN + "modified: " + Console.RESET + id.name)
-  case other          => other
-}
+def censorIdentifiers[T: Copy](t: T): T = t
+  .copyWith {
+    Mod.collect[Identifier] { id =>
+      id.copy(name = s"""${Console.GREEN}modified:${Console.RESET} ${id.name}""")
+    }
+  }
 
 @main def main(): Unit = {
   given Copy[RecIdent] = Copy.derived
