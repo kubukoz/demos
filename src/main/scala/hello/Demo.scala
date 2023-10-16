e8377a81bc6a74c6b119926b047125d6686208a5
Jakub Kozłowski <kubukoz@gmail.com>
2023-10-17 00:36:02 +0200

    Reproduce another issue

--- src/main/scala/hello/Demo.scala
+++ src/main/scala/hello/Demo.scala
@@ -7,7 +7,7 @@ import hello.WeatherService
 
 object Demo {
   val b =
-    SimpleRestJsonBuilder(WeatherService).client(??? : Client[IO])
+    SimpleRestJsonBuilder(WeatherServiceGen).client(??? : Client[IO])
 
   b
 }
