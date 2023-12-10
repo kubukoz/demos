a3a4a6cb80a6f6be13ca92cb9eac64feae1b7466
Jakub Kozłowski <kubukoz@gmail.com>
2023-12-10 01:12:21 +0100

    remove db

--- app/.native/src/main/scala/demo/Main.scala
+++ app/.native/src/main/scala/demo/Main.scala
@@ -2,27 +2,38 @@ package demo
 
 import libsqlite.all._
 
+import scala.scalanative.unsafe.Zone
+
 object Main {
 
   def main(
     args: Array[String]
-  ): Unit = {
-
+  ): Unit = Zone { case given Zone =>
     import scalanative.unsafe._
-    val db = stackalloc[Ptr[sqlite3]](1)
-
-    val ppStmt = stackalloc[Ptr[sqlite3_stmt]](1)
-    val pzTail = stackalloc[Ptr[CChar]](1)
+    val db = alloc[Ptr[sqlite3]](1)
+    val ppStmt = alloc[Ptr[sqlite3_stmt]](1)
+    val pzTail = alloc[Ptr[CChar]](1)
 
     sqlite3_open(c"hello", db)
     try {
-      sqlite3_prepare(!db, c"select 42", "select 42".length(), ppStmt, pzTail)
+      var q = "create table if not exists hello (a int, b int, c int)"
 
+      sqlite3_prepare_v2(!db, toCString(q), q.length, ppStmt, pzTail)
+      sqlite3_step(!ppStmt)
+
+      q = "insert into hello values(2,1,37)"
+
+      sqlite3_prepare_v2(!db, toCString(q), q.length, ppStmt, pzTail)
+      sqlite3_step(!ppStmt)
+
+      q = "select count(*) from hello"
+
+      sqlite3_prepare_v2(!db, toCString(q), q.length, ppStmt, pzTail)
       sqlite3_step(!ppStmt)
 
       println(sqlite3_column_int(!ppStmt, 0))
 
-    } finally sqlite3_close(!db)
+    } finally sqlite3_close_v2(!db)
   }
 
 }
