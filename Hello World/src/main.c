88453c48bdd78cbfafc99f481bb79b00a77c9f40
Jakub Kozłowski <kubukoz@gmail.com>
2023-12-10 04:30:17 +0100

    more fun

--- Hello World/src/main.c
+++ Hello World/src/main.c
@@ -6,15 +6,20 @@
 //  Copyright (c) 2014 Panic, Inc. All rights reserved.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "pd_api.h"
+#include <stdio.h>
 
 static int update(void *userdata);
 const char *fontpath = "/System/Fonts/Asheville-Sans-14-Bold.pft";
 LCDFont *font = NULL;
 
+int clickedGowno = 0;
+void menuItemCallback(void *userdata)
+{
+  clickedGowno = 1;
+}
+
+int clickedA = 0;
 #ifdef _WINDLL
 __declspec(dllexport)
 #endif
@@ -24,6 +29,8 @@ __declspec(dllexport)
 
   if (event == kEventInit)
   {
+
+    PDMenuItem *menuItem = pd->system->addMenuItem("gowno", menuItemCallback, pd);
     const char *err;
     font = pd->graphics->loadFont(fontpath, &err);
 
@@ -34,6 +41,11 @@ __declspec(dllexport)
     // Note: If you set an update callback in the kEventInit handler, the system
     // assumes the game is pure C and doesn't run any Lua code in the game
     pd->system->setUpdateCallback(update, pd);
+    // clickedA = 0;
+  }
+  else if (event == kEventKeyPressed)
+  {
+    clickedA = 1;
   }
 
   return 0;
@@ -56,19 +68,38 @@ static int update(void *userdata)
   pd->graphics->clear(kColorWhite);
   pd->graphics->setFont(font);
 
-  pd->graphics->drawText("GOWNO", strlen("GOWNO"), kUTF8Encoding, 100, 0);
+  pd->display->setInverted((color / 10) % 2);
+  color += 1;
+
+  char *str = clickedGowno ? "GOWNO CLICKED" : "GOWNO";
+
+  PDButtons *current = malloc(sizeof(PDButtons));
+  PDButtons *pushed = malloc(sizeof(PDButtons));
+
+  pd->system->getButtonState(current, pushed, NULL);
+
+  if ((*current & kButtonA) != 0)
+  {
+    clickedA = 1;
+  }
+  else if ((*pushed & kButtonA) != 0)
+  {
+    clickedA = 1;
+  }
+  else
+    clickedA = 0;
+
+  pd->graphics->drawText(str, strlen(str), kUTF8Encoding, 100, 0);
+  if (clickedA)
+    pd->graphics->drawText("A CLICKED", strlen("A CLICKED"), kUTF8Encoding, 100, 20);
 
   char *text = "UYSY CHUJ!";
   pd->graphics->drawText(text, strlen(text), kUTF8Encoding, x, y);
-  if ((color / 50) % 2 == 0)
-  {
-    pd->graphics->fillRect(0, 0, LCD_COLUMNS, LCD_ROWS, kColorBlack);
-  }
-  else
-  {
-    pd->graphics->fillRect(0, 0, LCD_COLUMNS, LCD_ROWS, kColorWhite);
-  }
-  color += 1;
+
+  pd->graphics->drawEllipse(50, 50, 50, 50, 2, 0, 0, kColorBlack);
+  pd->graphics->drawEllipse(150, 50, 50, 50, 2, 0, 0, kColorBlack);
+  pd->graphics->drawEllipse(250, 50, 50, 50, 2, 0, 0, kColorBlack);
+  pd->graphics->drawEllipse(150, 150, 50, 50, 2, 0, 0, kColorBlack);
 
   x += dx;
   y += dy;
