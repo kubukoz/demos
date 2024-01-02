e66ce82c78b202b7501b9aa17f1f31935c8f06ac
Jakub Kozłowski <kubukoz@gmail.com>
2024-01-02 04:26:52 +0100

    try that last part

--- compile.sh
+++ compile.sh
@@ -93,10 +93,27 @@ FLAGS=(
   -I "$SDK_PATH"
   "$SDK_PATH/buildsupport/setup.c"
   -o
-  main.o
+  setup.o
 )
 
 $CC ${FLAGS[@]}
-# /usr/local/bin/arm-none-eabi-gcc -g3 -c -mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_USED=1 -O2 -falign-functions=16 -fomit-frame-pointer -gdwarf-2 -Wall -Wno-unused -Wstrict-prototypes -Wno-unknown-pragmas  -fverbose-asm -Wdouble-promotion -mword-relocations -fno-common -ffunction-sections -fdata-sections -Wa,-ahlms=build/main.lst -DTARGET_PLAYDATE=1 -DTARGET_EXTENSION=1  -MD -MP -MF build/dep/main.o.d -I . -I . -I /Users/kubukoz/Developer/PlaydateSDK/C_API src/main.c -o build/src/main.o
-# /usr/local/bin/arm-none-eabi-gcc -g3 -c -mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_USED=1 -O2 -falign-functions=16 -fomit-frame-pointer -gdwarf-2 -Wall -Wno-unused -Wstrict-prototypes -Wno-unknown-pragmas  -fverbose-asm -Wdouble-promotion -mword-relocations -fno-common -ffunction-sections -fdata-sections -Wa,-ahlms=build/setup.lst -DTARGET_PLAYDATE=1 -DTARGET_EXTENSION=1  -MD -MP -MF build/dep/setup.o.d -I . -I . -I /Users/kubukoz/Developer/PlaydateSDK/C_API /Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/setup.c -o build//Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/setup.o
-# /usr/local/bin/arm-none-eabi-gcc -g3 build/src/main.o build//Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/setup.o -nostartfiles -mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_USED=1 -T/Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/link_map.ld -Wl,-Map=build/pdex.map,--cref,--gc-sections,--no-warn-mismatch,--emit-relocs    -o build/pdex.elf
+
+# -g3 build/src/main.o build//Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/setup.o -nostartfiles -mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_USED=1 -T/Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/link_map.ld -Wl,-Map=build/pdex.map,--cref,--gc-sections,--no-warn-mismatch,--emit-relocs    -o build/pdex.elf
+FLAGS=(
+  -g3
+  main.o
+  setup.o
+  -nostartfiles
+  -mthumb
+  -mcpu=cortex-m7
+  -mfloat-abi=hard
+  -mfpu=fpv5-sp-d16
+  -D__FPU_USED=1
+  -T$SDK_PATH/buildsupport/link_map.ld
+  -Wl,-Map=build/pdex.map,--cref,--gc-sections,--no-warn-mismatch,--emit-relocs
+  -o
+  pdex.elf
+)
+
+$CC ${FLAGS[@]}
+
