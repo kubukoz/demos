0a570f800c418961e0cce478dd14b4e0f0b9c34d
Jakub Kozłowski <kubukoz@gmail.com>
2024-01-02 04:23:18 +0100

    more flags, second command

--- compile.sh
+++ compile.sh
@@ -20,8 +20,6 @@ SIM_FLAGS=(
 CC=clang
 
 FLAGS=(
-  # -isystem /Users/kubukoz/Downloads/foo/include/c++/v1
-  # -isystem /Users/kubukoz/Downloads/foo/include/ #A
   -g3
   -c
   -mthumb
@@ -39,11 +37,11 @@ FLAGS=(
   -Wno-unknown-pragmas
   -fverbose-asm
   -Wdouble-promotion
-  # -mword-relocations
+  -mword-relocations
   -fno-common
   -ffunction-sections
   -fdata-sections
-  # -Wa,-ahlms=build/main.lst
+  '-Wa,-ahlms=build/main.lst'
   -DTARGET_PLAYDATE=1
   -DTARGET_EXTENSION=1
   -MD
@@ -60,5 +58,45 @@ FLAGS=(
 
 mkdir -p dep
 $CC ${FLAGS[@]}
+
+FLAGS=(
+  -g3
+  -c
+  -mthumb
+  -mcpu=cortex-m7
+  -mfloat-abi=hard
+  -mfpu=fpv5-sp-d16
+  -D__FPU_USED=1
+  -O2
+  -falign-functions=16
+  -fomit-frame-pointer
+  -gdwarf-2
+  -Wall
+  -Wno-unused
+  -Wstrict-prototypes
+  -Wno-unknown-pragmas
+  -fverbose-asm
+  -Wdouble-promotion
+  -mword-relocations
+  -fno-common
+  -ffunction-sections
+  -fdata-sections
+  '-Wa,-ahlms=build/setup.lst'
+  -DTARGET_PLAYDATE=1
+  -DTARGET_EXTENSION=1
+  -MD
+  -MP
+  -MF
+  dep/setup.o.d
+  -I .
+  -I .
+  -I "$SDK_PATH"
+  build/setup.c
+  -o
+  main.o
+)
+
+$CC ${FLAGS[@]}
+# /usr/local/bin/arm-none-eabi-gcc -g3 -c -mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_USED=1 -O2 -falign-functions=16 -fomit-frame-pointer -gdwarf-2 -Wall -Wno-unused -Wstrict-prototypes -Wno-unknown-pragmas  -fverbose-asm -Wdouble-promotion -mword-relocations -fno-common -ffunction-sections -fdata-sections -Wa,-ahlms=build/main.lst -DTARGET_PLAYDATE=1 -DTARGET_EXTENSION=1  -MD -MP -MF build/dep/main.o.d -I . -I . -I /Users/kubukoz/Developer/PlaydateSDK/C_API src/main.c -o build/src/main.o
 # /usr/local/bin/arm-none-eabi-gcc -g3 -c -mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_USED=1 -O2 -falign-functions=16 -fomit-frame-pointer -gdwarf-2 -Wall -Wno-unused -Wstrict-prototypes -Wno-unknown-pragmas  -fverbose-asm -Wdouble-promotion -mword-relocations -fno-common -ffunction-sections -fdata-sections -Wa,-ahlms=build/setup.lst -DTARGET_PLAYDATE=1 -DTARGET_EXTENSION=1  -MD -MP -MF build/dep/setup.o.d -I . -I . -I /Users/kubukoz/Developer/PlaydateSDK/C_API /Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/setup.c -o build//Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/setup.o
 # /usr/local/bin/arm-none-eabi-gcc -g3 build/src/main.o build//Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/setup.o -nostartfiles -mthumb -mcpu=cortex-m7 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -D__FPU_USED=1 -T/Users/kubukoz/Developer/PlaydateSDK/C_API/buildsupport/link_map.ld -Wl,-Map=build/pdex.map,--cref,--gc-sections,--no-warn-mismatch,--emit-relocs    -o build/pdex.elf
