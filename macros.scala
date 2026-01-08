5b07a1bc781c66d8ad89c322a15e95a9df7e6efe
Jakub Kozłowski <kubukoz@gmail.com>
2026-01-08 17:53:10 +0100

    cleanup

--- macros.scala
+++ macros.scala
@@ -15,71 +15,55 @@ object macros {
   ): Expr[A] = {
     import quotes.reflect.*
 
-    // Build a map from ctx call positions to their pattern variable names
     val ctxToVarName = scala.collection.mutable.Map[Term, String]()
 
-    // First pass: traverse to find flatMap/map with ctx calls and extract var names
+    object CtxCall {
+      def unapply(tree: Term): Option[Term] = tree match {
+        case app @ Apply(TypeApply(Select(Ident("macros"), "ctx"), _), List(_)) => Some(app)
+        case _ => None
+      }
+    }
+
+    // Traverse to find flatMap/map with ctx calls and extract variable names
     def collectPatterns(tree: Term): Unit = tree match {
-      // Match: macros.ctx(...).flatMap(a => ...) or macros.ctx(...).map(a => ...)
       case Apply(
-            TypeApply(Select(ctxCall @ Apply(TypeApply(Select(Ident("macros"), "ctx"), _), _), methodName @ ("flatMap" | "map")), _),
-            List(lambdaBlock @ Block(List(defDef @ DefDef(_, List(TermParamClause(List(ValDef(varName, _, _)))), _, rhs)), _))
+            TypeApply(Select(ctxCall @ CtxCall(_), "flatMap" | "map"), _),
+            List(Block(List(DefDef(_, List(TermParamClause(List(ValDef(varName, _, _)))), _, rhs)), _))
           ) =>
         ctxToVarName(ctxCall) = varName
-        // Continue traversing the lambda body to find nested patterns
         rhs.foreach(collectPatterns)
 
-      case _ =>
-        tree match {
-          case Block(stats, expr) =>
-            stats.foreach {
-              case term: Term => collectPatterns(term)
-              case defDef: DefDef =>
-                defDef.rhs.foreach(collectPatterns)
-              case _ => ()
-            }
-            collectPatterns(expr)
-          case Apply(fun, args) =>
-            collectPatterns(fun)
-            args.foreach(collectPatterns)
-          case TypeApply(fun, _) =>
-            collectPatterns(fun)
-          case Select(qual, _) =>
-            collectPatterns(qual)
-          case Inlined(_, _, expr) =>
-            collectPatterns(expr)
+      case Block(stats, expr) =>
+        stats.foreach {
+          case term: Term => collectPatterns(term)
+          case defDef: DefDef => defDef.rhs.foreach(collectPatterns)
           case _ => ()
         }
+        collectPatterns(expr)
+      case Apply(fun, args) =>
+        collectPatterns(fun)
+        args.foreach(collectPatterns)
+      case TypeApply(fun, _) => collectPatterns(fun)
+      case Select(qual, _) => collectPatterns(qual)
+      case Inlined(_, _, expr) => collectPatterns(expr)
+      case _ => ()
     }
 
     collectPatterns(forComp.asTerm)
 
-    report.info(s"Total patterns collected: ${ctxToVarName.size}, vars: ${ctxToVarName.values.mkString(", ")}")
-
-    // Second pass: transform ctx calls using the collected mapping
-    val transformedVars = scala.collection.mutable.ListBuffer[String]()
-
+    // Transform ctx calls to return the variable name
     val transformer = new TreeMap {
       override def transformTerm(tree: Term)(owner: Symbol): Term = tree match {
-        case ctxCall @ Apply(TypeApply(Select(Ident("macros"), "ctx"), _), List(arg)) =>
+        case ctxCall @ CtxCall(_) =>
           ctxToVarName.get(ctxCall) match {
-            case Some(varName) =>
-              transformedVars += varName
-              '{ Some(${ Expr(varName) }) }.asTerm
-            case None =>
-              super.transformTerm(tree)(owner)
+            case Some(varName) => '{ Some(${ Expr(varName) }) }.asTerm
+            case None => super.transformTerm(tree)(owner)
           }
-
-        case _ =>
-          super.transformTerm(tree)(owner)
+        case _ => super.transformTerm(tree)(owner)
       }
     }
 
-    val transformed = transformer.transformTerm(forComp.asTerm)(Symbol.spliceOwner)
-
-    report.info(s"Collected ${ctxToVarName.size} patterns: ${ctxToVarName.values.mkString(", ")}. Transformed ${transformedVars.size}: ${transformedVars.mkString(", ")}")
-
-    transformed.asExprOf[A]
+    transformer.transformTerm(forComp.asTerm)(Symbol.spliceOwner).asExprOf[A]
   }
 
 }
