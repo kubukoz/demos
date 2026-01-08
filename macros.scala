65f9752c0616a751e2968fa4bda5496ada9a821c
Jakub Kozłowski <kubukoz@gmail.com>
2026-01-08 17:59:36 +0100

    warnings, format

--- macros.scala
+++ macros.scala
@@ -1,3 +1,4 @@
+//> using option -Wunused:all
 import scala.quoted.*
 
 object macros {
@@ -16,36 +17,46 @@ object macros {
     import quotes.reflect.*
 
     object CtxCall {
-      def unapply(tree: Term): Option[Term] = tree match {
-        case app @ Apply(TypeApply(Select(Ident("macros"), "ctx"), _), List(_)) => Some(app)
-        case _ => None
-      }
+      def unapply(tree: Term): Option[Term] =
+        tree match {
+          case app @ Apply(TypeApply(Select(Ident("macros"), "ctx"), _), List(_)) => Some(app)
+          case _                                                                  => None
+        }
     }
 
     // Single pass: collect patterns and transform in one go
-    val transformer = new TreeMap {
-      val ctxToVarName = scala.collection.mutable.Map[Term, String]()
+    val transformer =
+      new TreeMap {
+        val ctxToVarName = scala.collection.mutable.Map[Term, String]()
 
-      override def transformTerm(tree: Term)(owner: Symbol): Term = tree match {
-        // First, check if this is a flatMap/map with a ctx call - collect the pattern
-        case Apply(
-              TypeApply(Select(ctxCall @ CtxCall(_), "flatMap" | "map"), targs),
-              List(lambda @ Block(List(DefDef(name, List(TermParamClause(List(ValDef(varName, _, _)))), tpt, rhs)), closure))
-            ) =>
-          ctxToVarName(ctxCall) = varName
-          // Continue transforming this tree
-          super.transformTerm(tree)(owner)
+        override def transformTerm(tree: Term)(owner: Symbol): Term =
+          tree match {
+            // First, check if this is a flatMap/map with a ctx call - collect the pattern
+            case Apply(
+                  TypeApply(Select(ctxCall @ CtxCall(_), "flatMap" | "map"), _),
+                  List(
+                    Block(
+                      List(
+                        DefDef(_, List(TermParamClause(List(ValDef(varName, _, _)))), _, _)
+                      ),
+                      _,
+                    )
+                  ),
+                ) =>
+              ctxToVarName(ctxCall) = varName
+              // Continue transforming this tree
+              super.transformTerm(tree)(owner)
 
-        // Then, transform ctx calls using collected patterns
-        case ctxCall @ CtxCall(_) =>
-          ctxToVarName.get(ctxCall) match {
-            case Some(varName) => '{ Some(${ Expr(varName) }) }.asTerm
-            case None => super.transformTerm(tree)(owner)
+            // Then, transform ctx calls using collected patterns
+            case ctxCall @ CtxCall(_) =>
+              ctxToVarName.get(ctxCall) match {
+                case Some(varName) => '{ Some(${ Expr(varName) }) }.asTerm
+                case None          => super.transformTerm(tree)(owner)
+              }
+
+            case _ => super.transformTerm(tree)(owner)
           }
-
-        case _ => super.transformTerm(tree)(owner)
       }
-    }
 
     transformer.transformTerm(forComp.asTerm)(Symbol.spliceOwner).asExprOf[A]
   }
