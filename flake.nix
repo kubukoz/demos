2a47a99567a75a02d33d0f34b496043bd8596c46
Jakub Kozłowski <kubukoz@gmail.com>
2026-04-04 21:07:11 +0200

    bump sdk

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
           name = "clang-nix-shell";
           shellHook = ''
             # Works on Mac
-            export PLAYDATE_SDK_PATH=~/Developer/PlaydateSDK-2.1.1
+            export PLAYDATE_SDK_PATH=~/Developer/PlaydateSDK-3.0.3
             # Works for me.
             export PLAYDATE_DEVICE_PATH=/dev/cu.usbmodemPDU1_Y0669441
 
